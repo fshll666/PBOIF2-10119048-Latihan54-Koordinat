0bbaec7ad6c4f0690734f9160309593cc0d4b155
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-11-21 20:18:01 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan54-Koordinat/src/pboif2/pkg10119048/latihan54/koordinat/WarnaKoordinat.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan54.koordinat;
+
+/**
+ *
+ * @author SystemOs
+ */
+public class WarnaKoordinat extends Koordinat{
+    public String namaWarna;
+
+    public WarnaKoordinat(int x, int y, String namaWarna) {
+        super(x, y);
+        this.namaWarna = namaWarna;
+    }
+
+    public String getNamaWarna() {
+        return namaWarna;
+    }
+
+    public void setNamaWarna(String namaWarna) {
+        this.namaWarna = namaWarna;
+    }
+}
+
